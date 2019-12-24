8ead013ab5ebe7c0b0a884e213b59c320635a80b
Fazlet <ed.fazletdinov@gmail.com>
2019-12-24 20:50:37 +0300

    Deleted intermediate compilation files. It compiles and works.

--- Build/Debug/Src/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/eduard/PathPlanningProject/Src
-# Build directory: /home/eduard/PathPlanningProject/Build/Debug/Src
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
